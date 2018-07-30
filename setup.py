6ea8a34d6d9051212484905c1848cb93dad42f27
vladsaveliev <vladislav.sav@gmail.com>
2018-07-30 12:18:34 +1000

    MultiQC: initial work. Copy QC data and re-generate bcbio report.

--- setup.py
+++ setup.py
@@ -20,4 +20,17 @@ setup(
         'scripts/pcgr',
     ],
     include_package_data=True,
+    
+    # For MultiQC_umccr
+    entry_points = {
+        'multiqc.templates.v1': [
+            'umccr = umccrise.multiqc.templates.umccr',
+        ],
+        'multiqc.hooks.v1': [
+            'config_loaded            = umccrise.multiqc.multiqc_umccr:config_loaded',
+            'execution_start          = umccrise.multiqc.multiqc_umccr:execution_start',
+            'after_modules            = umccrise.multiqc.multiqc_umccr:before_set_general_stats_html',
+            'before_report_generation = umccrise.multiqc.multiqc_umccr:after_set_general_stats_html',
+        ]
+    },
 )
